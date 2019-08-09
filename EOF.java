c0dfc8dd91095870a91239c4a30ee09601eb76a6
marcomereu2015 <53127114+marcomereu2015@users.noreply.github.com>
2019-08-09 08:15:10 -0500

    Add files via upload

---
+++ EOF.java
@@ -0,0 +1,17 @@
+#Hackerrank
+#Java - Java End-of-file
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        int i=1;
+        while (scanner.hasNextLine()) {
+            System.out.println(i++ + " " +scanner.nextLine());
+        }
+    }
+}
